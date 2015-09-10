5126a334bb62533250d9f501e06c2abd57840e3b
jmd025 <jmdllmm25@gmail.com>
2015-09-10 10:21:51 +0800

    tidy code

--- findapi.js
+++ findapi.js
@@ -1,13 +1,13 @@
 function __findScormAPI__(win, apiName) {
-win = win ? win : window;
-apiName = apiName ? apiName : "API_1484_11";
-var api = null;
-while (win != null) {
-api = win[apiName];
-if (api) {
-break;
-}
-win = (win.parent == win) ? win.opener : win.parent;
-}
-return api;
+    win = win ? win : window;
+    apiName = apiName ? apiName : "API_1484_11";    //use scorm1.3 as default
+    var api = null;
+    while (win != null) {
+        api = win[apiName];
+        if (api) {
+          break;
+        }
+        win = (win.parent == win) ? win.opener : win.parent;
+    }
+    return api;
 }
