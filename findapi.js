0d10cdaeac75c9b53258117742804ecb56dfe69c
jmd025 <jmdllmm25@gmail.com>
2016-02-15 16:13:54 +0800

    Update findapi.js

--- findapi.js
+++ findapi.js
@@ -2,11 +2,7 @@ function __findScormAPI__(win, apiName) {
     win = win ? win : window;
     apiName = apiName ? apiName : "API_1484_11";    //use scorm1.3 as default
     var api = null;
-    while (win != null) {
-        api = win[apiName];
-        if (api) {
-          break;
-        }
+    while (win != null && !(api = win[apiName])) {
         win = (win.parent == win) ? win.opener : win.parent;
     }
     return api;
