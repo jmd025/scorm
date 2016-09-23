cd519897cc12819c663fc54055e679e0774cec26
mindong.ji <jmd025@gmail.com>
2016-09-23 16:34:30 +0800

    updates

--- README.md
+++ README.md
@@ -5,4 +5,12 @@ A scorm toolkit that includind helper sciprts,htmls.
 A common scorm script that including find scorm api function etc.
 
 ## scorm.meta.cleaner.bat
-A bat script to clean scorm meta file,such as imsmanifest.xml/adlcp_v1p3.xsd files.
+A batch script to clean scorm meta file,such as imsmanifest.xml/adlcp_v1p3.xsd files.
+support scorm1.2 and scorm2004.
+
+usages:
+1.put the scorm.meta.cleaner.bat into the scorm course root folder and run it.
+
+2.drag the scorm course folder to the scorm.meta.cleaner.bat file.
+
+3.drag a file under scorm course root folder to the scorm.meta.cleaner.bat file.
--- scorm.meta.cleaner.bat
+++ scorm.meta.cleaner.bat
@@ -1,4 +1,23 @@
 @echo off
-set item="common","extend","unique","vocab","adlcp_v1p3.xsd","adlnav_v1p3.xsd","adlseq_v1p3.xsd","bdlcp_rootv1p2.xsd","courseMetadata.xml","datatypes.dtd","imscp_v1p1.xsd","imsmanifest.xml","imsss_v1p0.xsd","imsss_v1p0auxresource.xsd","imsss_v1p0control.xsd","imsss_v1p0delivery.xsd","imsss_v1p0limit.xsd","imsss_v1p0objective.xsd","imsss_v1p0random.xsd","imsss_v1p0rollup.xsd","imsss_v1p0seqrule.xsd","imsss_v1p0util.xsd","lom.xsd","lomCustom.xsd","lomLoose.xsd","lomStrict.xsd","xml.xsd","XMLSchema.dtd"
+set dest_folder=%~dp0
+
+if not "%1"=="" (
+	set dest_folder=%1
+)
+
+if exist %dest_folder% (
+	if not exist %dest_folder%\NUL (
+		set dest_folder=%~dp1
+	)
+)
+
+cd /d %dest_folder%
+
+set item="common","extend","unique","vocab","adlcp_v1p3.xsd","adlnav_v1p3.xsd","adlseq_v1p3.xsd","bdlcp_rootv1p2.xsd","courseMetadata.xml","datatypes.dtd","imscp_v1p1.xsd","imsmanifest.xml","imsss_v1p0.xsd","imsss_v1p0auxresource.xsd","imsss_v1p0control.xsd","imsss_v1p0delivery.xsd","imsss_v1p0limit.xsd","imsss_v1p0objective.xsd","imsss_v1p0random.xsd","imsss_v1p0rollup.xsd","imsss_v1p0seqrule.xsd","imsss_v1p0util.xsd","lom.xsd","lomCustom.xsd","lomLoose.xsd","lomStrict.xsd","xml.xsd","XMLSchema.dtd","adlcp_rootv1p2.xsd","ims_xml.xsd","imscp_rootv1p1p2.xsd","imsmd_rootv1p2p1.xsd"
 
 For %%i in (%item%) do del /f /q /s %%i & rd /q /s %%i
+
+echo ----------------------
+echo  Finished clean
+echo ----------------------
+ping 127.0.0.1 > NUL
